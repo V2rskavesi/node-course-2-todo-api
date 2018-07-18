b1a8893e1defdea2aa83105b5184525751b32a09
Hans <hans@budopunkt.ee>
2018-07-18 20:41:42 +0300

    mLab Heroku tests

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //there are several versions of promises, we use the built in one
-// mongoose.connect('mongodb://Testguy:testguy1@ds243041.mlab.com:43041/node-todo-api-mongodb');
-mongoose.connect('mongodb://Testguy:testguy1@ds243041.mlab.com:43041/node-todo-api-mongodb' ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
  module.exports = {mongoose};
+// mongodb://Testguy:testguy1@ds243041.mlab.com:43041/node-todo-api-mongodb

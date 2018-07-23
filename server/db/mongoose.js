b71415ab6bf838717a3bfc6a395f07b697c6d981
Hans <hans@budopunkt.ee>
2018-07-24 01:08:27 +0300

    Set up seperate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //there are several versions of promises, we use the built in one
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-// mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(process.env.MONGODB_URI);
  module.exports = {mongoose};
 // mongodb://Testguy:testguy1@ds243041.mlab.com:43041/node-todo-api-mongodb
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config.js')
 var _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -8,7 +9,8 @@ var {User} = require('./models/user');
 const {ObjectID} = require('mongodb')
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 'developement';
+
 app.use(bodyParser.json()); //middleware
 
 app.post('/todos',(req,res) => { //
